e347bfa12e473158b3fc83463c9119f4b0c5f3bb
Oblosys <martijn@oblomov.com>
2003-12-11 13:22:44 +0000

    Imported source
    git-svn-id: svn://svn.oblomov.com/Proxima@428 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

---
+++ thesis/trunk/texAll.bat
@@ -0,0 +1,8 @@
+@echo off
+rem for some reason, 'runLatex EntireThesis' does not return, so we call latex explicitly here
+
+latex EntireThesis.tex --src-specials=cr -quiet -halt-on-error -include-directory TexSources -include-directory Styles -include-directory TexOutput -output-directory TexOutput
+
+copy TexOutput\EntireThesis.aux TexOutput\Thesis.aux
+rem .aux is copied, so Thesis can be TeXed with the right chapter references.
+rem .toc is not copied, so Thesis.tex must have been TeXed before running texAll, to get the toc right
