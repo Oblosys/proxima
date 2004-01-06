46ba132578723f1bb4f1eb736cfb6730ac3b07d6
Oblosys <martijn@oblomov.com>
2004-01-06 13:25:46 +0000

    uncommented dvips
    git-svn-id: svn://svn.oblomov.com/Proxima@450 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- thesis/trunk/publishAs.bat
+++ thesis/trunk/publishAs.bat
@@ -4,6 +4,6 @@ echo # Creating %1.dvi, %1.pdf, and %1.ps from latest.dvi
 echo #
 cd D:\Data\documents\ThesisCVS\TexSources
 copy latest.dvi D:\Data\documents\ThesisCVS\TexOutput\%1.dvi
-rem dvips -o D:\Data\documents\ThesisCVS\TexOutput\%1.ps latest.dvi
+dvips -o D:\Data\documents\ThesisCVS\TexOutput\%1.ps latest.dvi
 dvipdfm -p a4 -o D:\Data\documents\ThesisCVS\TexOutput\%1.pdf latest.dvi
 cd ..
