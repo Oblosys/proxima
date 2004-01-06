ad950e152339800d054fcef7cc0d0eeb49bf39c7
Oblosys <martijn@oblomov.com>
2004-01-06 13:20:18 +0000

    imported source
    git-svn-id: svn://svn.oblomov.com/Proxima@444 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

---
+++ thesis/trunk/bibAll.bat
@@ -0,0 +1,1 @@
+runbib EntireThesis 
---
+++ thesis/trunk/cleanTex.bat
@@ -0,0 +1,9 @@
+del TexSource\*.dvi
+del TexOutput\*.dvi
+del TexOutput\*.aux
+del TexOutput\*.log
+del TexOutput\*.toc
+del TexOutput\*.blg
+del TexOutput\*.bbl
+del TexOutput\*.out
+
