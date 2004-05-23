7934c4d3c3599244d48555da7b2831c9def348aa
Oblosys <martijn@oblomov.com>
2004-05-23 16:10:45 +0000

    stable version
    git-svn-id: svn://svn.oblomov.com/Proxima@112 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- trunk/test/Heliumfile.hs
+++ trunk/test/Heliumfile.hs
@@ -1,2 +1,12 @@
 
-x = 1*2+3;
+x = let f = 1; in ( 1+2+3, [1,2,3,4], [4,5,6] );
+
+spelling = evem 3; 
+
+list = [ True, 1, False, 3, True ];
+
+test = f 3;
+
+s = \f -> \g -> \x -> f x (g x);
+
+f = \x -> x^2+2*x+(3+x)*(2+x)*1%(x+1)^2;
