8dabce6f2c8ca9e6c021d6355e8ea8eed73c1ce5
Oblosys <martijn@oblomov.com>
2004-06-09 20:17:46 +0000

    allerlei updates
    git-svn-id: svn://svn.oblomov.com/Proxima@516 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- thesis/trunk/cnv.bat
+++ thesis/trunk/cnv.bat
@@ -1,3 +1,7 @@
-cd TexSources\Pics
-for  %%f  in  (*.*)   do d:\bin\imagemagick\convert.exe   %%f eps\%%f.eps 
-cd ..\..
+rem cd TexSources\Pics
+rem for  %%f  in  (*.*)   do d:\bin\imagemagick\convert.exe   %%f eps\%%f.eps 
+rem cd ..\..
+
+cd TexSources\Pics\Screenshots
+for  %%f  in  (*.png)   do d:\bin\imagemagick\convert.exe %%f %%f.eps 
+cd ..\..\..
--- thesis/trunk/texAll.bat
+++ thesis/trunk/texAll.bat
@@ -6,6 +6,6 @@ rem bibAll
 
 
 
-latex EntireThesis.tex --src-specials=cr  -include-directory TexSources -include-directory Styles -include-directory TexOutput -output-directory TexOutput | grep -E "defined|rerun"
+latex EntireThesis.tex --src-specials=cr  -include-directory TexSources -include-directory Styles -include-directory TexOutput -output-directory TexOutput | grep -E "defined|rerun|Overfull|TexSources"
 
-rem show undefined and multiply defined labels + rerun warnings
+rem show undefined and multiply defined labels + rerun warnings, overfull boxes, and input filenames
