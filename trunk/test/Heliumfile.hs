a5865bd3d9f79205fc50f2f0131e63880df12a75
Oblosys <martijn@oblomov.com>
2005-01-11 12:47:56 +0000

    no changes, cvs test
    git-svn-id: svn://svn.oblomov.com/Proxima@124 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- trunk/test/Heliumfile.hs
+++ trunk/test/Heliumfile.hs
@@ -1,4 +1,8 @@
 
+Inv: inv toc < ( < "bla" , #1 > : ( < "bla" , #2 > : ( < "blaa" , #3 > : [] ) ) ) , ( "bla" : ( "bla" : ( "blaa" : [] ) ) ) >
+
+Inv: inv mySnoc ( "a" : ("b" : ("c" : [] ) ) ) 
+
 inc = \x -> x+1;
 
 h = [(1,False),(2,True),(3,False)];
@@ -7,7 +11,7 @@ s = \f -> \g -> \x -> f x (g x);
 
 x = let local = 1+2+3+4; h = 1; in if True then local + h else 0;
 
-test = f 3;
+test = f 4;
 
 f = \x -> x^2+2*x+(3+x)*(2+x)*1%(x+1)^2;
 
