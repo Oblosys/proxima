ea812c0d8dfc1a8020497d3eee40a7037433a1c4
Oblosys <martijn@oblomov.com>
2005-06-30 12:52:27 +0000

    Initial revision
    git-svn-id: svn://svn.oblomov.com/Proxima@128 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

---
+++ trunk/scripts/cleanAll.bat
@@ -0,0 +1,18 @@
+del ..\src\Arrangement\*.o
+del ..\src\Common\*.o
+del ..\src\Evaluation\*.o
+del ..\src\Layout\*.o
+del ..\src\Main\*.o
+del ..\src\Presentation\*.o
+del ..\src\Rendering\*.o
+
+del ..\src\Arrangement\*.hi
+del ..\src\Common\*.hi
+del ..\src\Evaluation\*.hi
+del ..\src\Layout\*.hi
+del ..\src\Main\*.hi
+del ..\src\Presentation\*.hi
+del ..\src\Rendering\*.hi
+
+del /s ..\..\*.o
+del /s ..\..\*.hi
