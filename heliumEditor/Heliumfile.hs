1f304257b00c4b2b32afe4930184c10ac8bf6bb3
Oblosys <martijn@oblomov.com>
2005-12-13 15:02:35 +0000

    haakjes
    git-svn-id: svn://svn.oblomov.com/Proxima@257 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- heliumEditor/Heliumfile.hs
+++ heliumEditor/Heliumfile.hs
@@ -6,6 +6,8 @@ x = let local = 1+2+3+4; h = 1; in if True then local + h else 0;
 
 g = case 5*7 of aaa -> 2+4; b -> 10;;
 
+haakjes = (((((1)))));
+
 test = f 3;
 
 f = \x -> x^2+2*x+(3+x)*(2+x)*1%(x+1)^2;
