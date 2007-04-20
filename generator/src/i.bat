4462d984deb2cddd242f1236d4f210d1881ce07c
Oblosys <martijn@oblomov.com>
2007-04-20 12:43:01 +0000

    Compiled by makeFile now
    git-svn-id: svn://svn.oblomov.com/Proxima@318 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- generator/src/i.bat
+++
@@ -1,1 +0,0 @@
-ghci Generator.hs -package lang  -package util -fglasgow-exts -i../../../uust/lib/parsing -i../../../uust/lib/scanner -i../../../uust/lib/util
--- generator/src/make.bat
+++
@@ -1,1 +0,0 @@
-ghc Generator.hs --make -package lang -fglasgow-exts -o generate -i../../../uust/lib/parsing -i../../../uust/lib/scanner -i../../../uust/lib/util
