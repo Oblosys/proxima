086ad472042bf4167443efb274272bc297275449
Oblosys <martijn@oblomov.com>
2007-05-10 14:44:28 +0000

    files toegevoegd die niet in CVS zaten
    git-svn-id: svn://svn.oblomov.com/Proxima@555 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

---
+++ thesis/trunk/cnv'.bat
@@ -0,0 +1,15 @@
+cd TexSources\Pics\Screenshots
+d:\bin\imagemagick\convert.exe strEditBuild1.png strEditBuild1.png.eps
+d:\bin\imagemagick\convert.exe strEditBuild2.png strEditBuild2.png.eps
+d:\bin\imagemagick\convert.exe strEditBuild3.png strEditBuild3.png.eps
+d:\bin\imagemagick\convert.exe presEdit1.png presEdit1.png.eps
+d:\bin\imagemagick\convert.exe presEdit2.png presEdit2.png.eps
+
+d:\bin\imagemagick\convert.exe strEditBuild2Y.png strEditBuild2Y.png.eps
+d:\bin\imagemagick\convert.exe strEditBuild3Y.png strEditBuild3Y.png.eps
+d:\bin\imagemagick\convert.exe presEdit1Y.png presEdit1Y.png.eps
+d:\bin\imagemagick\convert.exe presEdit2Y.png presEdit2Y.png.eps
+
+cd ..\..\..
+
+
