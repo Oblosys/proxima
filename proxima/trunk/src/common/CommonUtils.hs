697aa38a05845642f1aa3b93fbb676518e67fd38
Oblosys <martijn@oblomov.com>
2007-05-30 17:45:45 +0000

    computeAngle now lies between 0 and 2*pi
    git-svn-id: svn://svn.oblomov.com/Proxima@618 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- proxima/trunk/src/common/CommonUtils.hs
+++ proxima/trunk/src/common/CommonUtils.hs
@@ -36,9 +36,17 @@ scaleInt scale x = x -- round (fromIntegral x * scale)
 descaleInt :: Double -> Int -> Int
 descaleInt scale x = x -- round (fromIntegral x / scale)
 
--- Compute angle of the line (fromx,fromy) (tox,toy)
+-- Compute angle of the line (fromx,fromy) (tox,toy), result lies in [0..2*pi>
 computeAngle :: Int -> Int -> Int -> Int -> Double
-computeAngle fromx fromy tox toy = atan (fromIntegral (toy-fromy)/fromIntegral (fromx-tox)) +
-                                   if fromx < tox then if fromy <= toy then pi else -pi else 0
--- toy and fromy switched because tan uses mathematical coordinate system (positive is up)
--- in upper left quadrant, add pi, and in lower left quadrant, subtract pi, so the resulting angle is between -pi and pi
+computeAngle fromx fromy tox toy = 
+  atan (fromIntegral (-(toy-fromy))/fromIntegral (tox-fromx)) + 
+  if tox < fromx then pi else if fromy < toy then 2* pi else 0 
+{-
+In quadrants  II  I  , atan a is between 0 and pi/2 or -pi/2:  - +
+              III IV                                           + -
+              
+Therefore, we add   pi 0   , based on the quadrant the line is pointing in.
+                    pi 2pi
+
+Note, remember that screen y is flipped wrt. mathematical y
+-}
