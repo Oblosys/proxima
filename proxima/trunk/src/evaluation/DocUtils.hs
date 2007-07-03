a7a7880d603496e01ed2c92cec83cf31f3d3f851
Oblosys <martijn@oblomov.com>
2007-07-03 14:02:37 +0000

    Moved some functions from DocUtils_Generated to generic DocUtils
    git-svn-id: svn://svn.oblomov.com/Proxima@651 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- proxima/trunk/src/evaluation/DocUtils.hs
+++ proxima/trunk/src/evaluation/DocUtils.hs
@@ -3,6 +3,7 @@ module DocUtils where
 
 import DocTypes
 import PresTypes
+import Text.ParserCombinators.Parsec
 
 redirect (SkipDoc i)     = (SkipDoc' i)
 redirect (SetDoc doc {- inssdels -})    = (SetDoc' doc {- inssdels -})
@@ -18,3 +19,89 @@ redirect PasteDoc        = PasteDoc'
 redirect DeleteDoc       = DeleteDoc'
 redirect EvaluateDoc     = EvaluateDoc'
 redirect _               = (SkipDoc' 0)
+
+
+
+
+data XML = Elt String [(String, String)] [XML] | PCData String | EmptyElt
+
+
+showXML xml = 
+{-     "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+  ++ case xml of (Elt tg _ _) -> "<!DOCTYPE "++tg++" SYSTEM \""++tg++".dtd\" >\n"
+                 _            -> ""
+  ++ -} showXML' 0 xml
+ where showXML' i (Elt tag ps []) = replicate i ' ' ++"<"++tag++showProperties ps++"/>\n"
+       showXML' i (Elt tag ps [PCData str]) = replicate i ' ' ++"<"++tag++showProperties ps++">"++str++"</"++tag++">\n" 
+       showXML' i (Elt tag ps cs) = replicate i ' ' ++"<"++tag++showProperties ps++">\n"
+                              ++ concatMap (showXML' (i+2)) cs
+                              ++ replicate i ' ' ++"</"++tag++">\n" 
+       showXML' i (EmptyElt)     = replicate i ' ' ++"Empty\n"
+       showXML' i (PCData str)   = replicate i ' ' ++str++"\n"
+       showProperties [] = ""
+       showProperties ((p,v):ps) = " "++p++"="++show v++ showProperties ps
+-- element with one child PCDATA is displayed on one line. For more than one child it's too much of a hassle
+-- because this function is only temporary
+
+toXMLBool True = Elt "True" [] [] 
+toXMLBool False = Elt "False" [] [] 
+
+toXMLInt i = Elt "Integer" [("val", show i)] []
+
+toXMLString str = Elt "String" [] [PCData str] 
+
+-- Parsing
+
+
+mkList listCns consCns nilCns lst = listCns NoIDD $ foldr consCns nilCns lst
+
+parseXML_String :: Parser String
+parseXML_String =
+ do { spaces
+    ; string "<String>"
+    ; str <- many (satisfy (/='<')) 
+    ; string "</String>"
+    ; return str
+    } 
+
+parseXML_Int :: Parser Int
+parseXML_Int  =
+ do { spaces
+    ; string "<Integer val=\""
+    ; str <- many (satisfy (/='"')) 
+    ; string "\"/>"
+    ; return $ read str
+    } 
+
+parseXML_Bool :: Parser Bool
+parseXML_Bool =
+ do { spaces
+    ; string "<Bool val=\""
+    ; str <- many (satisfy (/='"')) 
+    ; string "\"/>"
+    ; return $ read str
+    }
+     
+infixl 4 <*>, <$>, <$, <*
+
+(<*>) :: Parser (a -> b) -> Parser a -> Parser b
+p1 <*> p2 = do { f <- p1; a <- p2; return $ f a }
+
+(<*) :: Parser a -> Parser b -> Parser a
+p1 <* p2 = do { a <- p1; p2; return a }
+
+(<$>) :: (a -> b) -> Parser a -> Parser b
+(<$>) = fmap
+    
+(<$) :: b -> Parser a -> Parser b
+b <$ p = do { p; return b } 
+
+-- binary choice with try
+(<?|>) :: Parser a -> Parser a -> Parser a
+p1 <?|> p2 = choice [try p1, p2] 
+
+startTag eltName = do { spaces; string $ "<"++eltName++">"}
+endTag   eltName = do { spaces; string $ "</"++eltName++">"}
+emptyTag eltName = do { spaces; string $ "<"++eltName++"/>"}
+
+
