4a65b81d28fcb5d975de701a5d64d22946383980
Oblosys <martijn@oblomov.com>
2007-08-01 17:50:52 +0000

    git-svn-id: svn://svn.oblomov.com/Proxima@711 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- proxima/trunk/src/arrangement/Arranger.hs
+++ proxima/trunk/src/arrangement/Arranger.hs
@@ -23,7 +23,6 @@ arrangePresentation state fontMetricsRef focus oldArrangement dt pres =
   --  ; debugLnIO Err ("pruned presentation"++show pres')
     ; (attrTree, maxFDepth, unfoldedTree) <- fixed fontMetricsRef focus pres' screenSize viewedArea oldArrangement
  -- ; debugLnIO Arr ("  maxFormatterDepth = "++ show maxFDepth)   
-    ; putStrLn $ show attrTree
           
     ; if maxFDepth == 0 then
         return attrTree
