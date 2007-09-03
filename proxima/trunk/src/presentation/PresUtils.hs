5bff1a90a102801a3f2ddda3ba6f1c2fc81404d0
Oblosys <martijn@oblomov.com>
2007-09-03 17:27:18 +0000

    Comments
    git-svn-id: svn://svn.oblomov.com/Proxima@828 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- proxima/trunk/src/presentation/PresUtils.hs
+++ proxima/trunk/src/presentation/PresUtils.hs
@@ -227,11 +227,7 @@ prunePresSpecial va ova (x,y) dt arr                                   pres =
 -- TODO: check this!!
   
 -- this solves problem with incorrect nr of children in formatter unfolding
--- we still have the problem with incorrect refs in col of rows in oldArr of Formatter
--- old C R R  structure may not correspond to new one. In which case reuse is done wrong.
--- Solved by storing distribution in Formatted arg of ColP
--- Furthermore, arrangments moved into rows out of view will try to reuse from Poly.
--- Storing the arrangement in ArrangedP solves this problem.
+
 
 
 -- probably goes wrong when inserted is not direct child of row, col, or overlay
