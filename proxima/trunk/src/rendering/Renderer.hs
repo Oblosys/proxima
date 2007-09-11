a49c6c9f5e4e19878e7c66bb9f5020f43b7f2f35
Oblosys <martijn@oblomov.com>
2007-09-11 12:05:39 +0000

    turned off render when viewed
    git-svn-id: svn://svn.oblomov.com/Proxima@856 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- proxima/trunk/src/rendering/Renderer.hs
+++ proxima/trunk/src/rendering/Renderer.hs
@@ -170,8 +170,8 @@ renderArr oldClipRegion (wi,dw,gc) arrDb scale (lux, luy) viewedArea diffTree ar
                     ParsingA _ arr              -> renderChildren 0 0 [arr]
                     LocatorA _ arr              -> renderChildren 0 0 [arr]
                     _ -> return ()
-     else when (overlap ((lux+xA arrangement, luy+yA arrangement),
-                         (widthA arrangement, heightA arrangement)) viewedArea) $
+     else --when (overlap ((lux+xA arrangement, luy+yA arrangement),
+          --               (widthA arrangement, heightA arrangement)) viewedArea) $
           -- only render when the arrangement is in the viewed area   
   case arrangement of 
 
@@ -191,6 +191,7 @@ renderArr oldClipRegion (wi,dw,gc) arrDb scale (lux, luy) viewedArea diffTree ar
                     ; drawFilledRectangle dw gc (Rectangle x y w h) bgColor bgColor
                     }
            
+              ; putStrLn $ show (x,y,w,h)
               ; context <- widgetCreatePangoContext wi
               ; fontDescription <- fontDescriptionFromProximaFont fnt
               
