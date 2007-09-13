630a7ce74a587268329430fdd22900ff4ead25c6
Oblosys <martijn@oblomov.com>
2007-09-13 11:08:51 +0000

    Removed old multi-pass AG stuff
    git-svn-id: svn://svn.oblomov.com/Proxima@869 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- proxima/trunk/src/arrangement/Arranger.hs
+++ proxima/trunk/src/arrangement/Arranger.hs
@@ -24,7 +24,7 @@ arrangePresentation :: Show node => LocalStateArr -> FontMetricsRef -> FocusPres
                        DiffTree -> Presentation doc node clip -> IO (Arrangement node, LocalStateArr)
 arrangePresentation state fontMetricsRef focus oldArrangement dt pres =
 
- do { viewedArea <- readIORef $getViewedAreaRef state
+ do { viewedArea <- readIORef $ getViewedAreaRef state
     ; let lastViewedArea = getLastViewedArea state
           state' = state { getLastViewedArea = viewedArea }
 --          prunedPres = prunePres viewedArea lastViewedArea (0,0) dt oldArrangement pres
@@ -82,62 +82,3 @@ fixed fontMetricsRef focus (pres :: Presentation doc node clip) (unprunedPres ::
             ; return (updatedMetrics, arrangement, maxFDepth)
             }
             
-
-{-
-fixed :: Show node => FontMetricsRef -> FocusPres -> Presentation doc node clip -> Rectangle -> 
-         Arrangement node -> IO (Arrangement node, Integer, Presentation doc node clip)
-fixed fontMetricsRef focus (pres :: Presentation doc node clip) viewedArea oldArrangement = 
- f 
- {- mdo { (arrangement, maxFDepth, unfoldedTree) <- f (arrangement, maxFDepth, unfoldedTree)
-    ; return (arrangement, maxFDepth, unfoldedTree)
-    } -}
- where --f :: [Font] -> IO ([Font], Arrangement node, Integer, Presentation doc node clip) -- doc and node are scoped type variables
-       f  = 
-         do { putStr "Fonts"
-            ; let (allFonts, arrangement, maxFDepth, unfoldedTree) =
-                    sem_Root (Root pres) [defaultFont]
-                                               defaultBackColor defaultFillColor
-                                               focus
-                                               defaultFont 
-                                               Map.empty
-                                               defaultLineColor
-                                               Nothing  -- mouseDown : Maybe (UpdateDoc doc clip)
-                                               oldArrangement
-                                               []       -- popupMenu : [String, (UpdateDoc doc clip)] 
-                                               defaultTextColor
-                                               viewedArea
-               
-            ; let usedFonts = nub allFonts
-             
-           -- ; debugLnIO Arr ("The fonts are:"++show usedFonts)
-            ; queriedMetrics <- readIORef fontMetricsRef
-            
-            ; let queriedFonts = Map.keys queriedMetrics
-            ; let newFonts =  deleteFirstsBy (==) usedFonts queriedFonts -- usedFonts was nubbed
-{-
-            ; debugLnIO Arr $ "used: "           ++ show usedFonts
-            ; debugLnIO Arr $ "already queried: "++ show queriedFonts
-            ; debugLnIO Arr $ "new:             "++ show newFonts
--}
-            ; newMetrics <- mkFontMetrics newFonts
-            ; let updatedMetrics = newMetrics `Map.union` queriedMetrics
-            ; writeIORef fontMetricsRef updatedMetrics            
- ; putStr "Arranging"
-            ; 
-           ; let (allFonts, arrangement,  maxFDepth, unfoldedTree) =
-                    sem_Root (Root pres) [defaultFont]
-                                          defaultBackColor defaultFillColor
-                                          focus
-                                          defaultFont
-                                          updatedMetrics
-                                          defaultLineColor
-                                          Nothing
-                                          oldArrangement
-                                          []
-                                          defaultTextColor
-                                          viewedArea 
- 
-            ; return ( arrangement, maxFDepth, unfoldedTree)
-            }
--}
-
