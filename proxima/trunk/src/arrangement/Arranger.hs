59dfe095c9a60207cc90ed48d349a202530c072d
Oblosys <martijn@oblomov.com>
2007-09-13 09:44:07 +0000

    New arranger incrementality seems to work! (except for scrolling)
    git-svn-id: svn://svn.oblomov.com/Proxima@866 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- proxima/trunk/src/arrangement/Arranger.hs
+++ proxima/trunk/src/arrangement/Arranger.hs
@@ -24,24 +24,25 @@ arrangePresentation :: Show node => LocalStateArr -> FontMetricsRef -> FocusPres
                        DiffTree -> Presentation doc node clip -> IO (Arrangement node, LocalStateArr)
 arrangePresentation state fontMetricsRef focus oldArrangement dt pres =
 
- do { viewedArea <- readIORef $ getViewedAreaRef state
+ do { viewedArea <- readIORef $getViewedAreaRef state
     ; let lastViewedArea = getLastViewedArea state
           state' = state { getLastViewedArea = viewedArea }
-          prunedPres = prunePres viewedArea lastViewedArea (0,0) dt oldArrangement pres
+--          prunedPres = prunePres viewedArea lastViewedArea (0,0) dt oldArrangement pres
+          prunedPres = prunePres dt pres
     --; putStrLn $ "Viewed area: "++show viewedArea ++ " last viewed area: "++show lastViewedArea
     --; debugLnIO Err ("Diff tree"++show dt)
     --; debugLnIO Err ("Presentation"++show pres)
-    --; debugLnIO Err ("Pruned Presentation"++show pres')
-    ; (attrTree, maxFDepth,_) <- fixed fontMetricsRef focus prunedPres pres viewedArea oldArrangement
+    --; debugLnIO Err ("Pruned Presentation"++show prunedPres)
+    ; (attrTree, maxFDepth,_) <- fixed fontMetricsRef focus prunedPres pres viewedArea lastViewedArea oldArrangement
     ; if maxFDepth > 1 
       then debugLnIO Err "Nested formatters may be arranged incorrectly"
       else return ()
     ; return (attrTree, state')
     }
 
-fixed :: Show node => FontMetricsRef -> FocusPres -> Presentation doc node clip -> Presentation doc node clip -> Rectangle -> 
+fixed :: Show node => FontMetricsRef -> FocusPres -> Presentation doc node clip -> Presentation doc node clip -> Rectangle -> Rectangle -> 
          Arrangement node -> IO (Arrangement node, Integer, Presentation doc node clip)
-fixed fontMetricsRef focus (pres :: Presentation doc node clip) (unprunedPres :: Presentation doc node clip) viewedArea oldArrangement = 
+fixed fontMetricsRef focus (pres :: Presentation doc node clip) (unprunedPres :: Presentation doc node clip) viewedArea oldViewedArea oldArrangement = 
  mdo { (fontMetrics,arrangement, maxFDepth, unfoldedTree) <- f (fontMetrics,arrangement, maxFDepth, unfoldedTree)
     ; return (arrangement, maxFDepth, unfoldedTree)
     }
@@ -57,6 +58,7 @@ fixed fontMetricsRef focus (pres :: Presentation doc node clip) (unprunedPres ::
                                                defaultLineColor
                                                Nothing  -- mouseDown : Maybe (UpdateDoc doc clip)
                                                oldArrangement
+                                               oldViewedArea
                                                []       -- popupMenu : [String, (UpdateDoc doc clip)] 
                                                defaultTextColor
                                                unprunedPres
