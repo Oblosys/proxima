bd7eef1081aa96b83593180db95f91de68b6eac7
Oblosys <martijn@oblomov.com>
2008-02-04 16:57:12 +0000

    added whitespace for structural presentations div and pow
    git-svn-id: svn://svn.oblomov.com/Proxima@1078 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- heliumEditor/Heliumfile.hs
+++ heliumEditor/Heliumfile.hs
@@ -1,14 +1,2 @@
-inc = \x -> x+1;
-
-h = [(1,False),(2,True),(3,False)];
-
-x = let local = 1+2+3+4; h = 1; in if True then local + h else 0;
-
-g = case 5*7 of aaa -> 2+4; b -> 10;;
-
-h1 = ((((((((((1))))))))));
-h2 = (let x = 1*2+3;in ((4)+5*(6+7),8));
-h2 = case x of a -> (((1)));;
-
-test = f 3;
+inc = 1%2;
 
