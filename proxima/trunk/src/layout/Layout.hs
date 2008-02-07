c3313e5aad5a8d76d844cc9ebe2737615300d4b9
Oblosys <martijn@oblomov.com>
2008-02-07 09:09:27 +0000

    comments and var renaming
    git-svn-id: svn://svn.oblomov.com/Proxima@1102 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- proxima/trunk/src/layout/Layout.hs
+++ proxima/trunk/src/layout/Layout.hs
@@ -13,73 +13,71 @@ import Data.Map (Map)
 -- mutually recursive functions: detokenize for walking the tree until ParsingP node
 --                               detokenize' for adding whitespace until StructureP node
 
--- Local State is layout, inserted and deleted state
+-- Local State is layout
 
 -- detokenize ignores Lexer information, since alle tokens can be treated the same when layouting.
--- Note that freetext tokens have no layout in the extra state, but since their id's will not be
--- in the WhitespaceMap, nothing will be inserted.
 detokenize :: Show token => WhitespaceMap -> Presentation doc node clip token -> Layout doc node clip
-detokenize lm (ParsingP id l pres)       = let press = detokenize' lm pres
+detokenize wm (ParsingP id l pres)       = let press = detokenize' wm pres
                                            in  if null press 
                                                then debug Err ("TreeEditPres.detokenize empty token list") (StringP NoIDP "") 
                                                else if length press == 1  -- this will usually be the case because breaks
                                                then ParsingP id l $ head press -- are produced by col's so there will be one there
                                                else ParsingP id l $ ColP NoIDP 0 NF press 
-detokenize lm (EmptyP id)                = EmptyP id
-detokenize lm (StringP id str)           = StringP id str
-detokenize lm (ImageP id str st)         = ImageP id str st
-detokenize lm (PolyP id pts w st)        = PolyP id pts w st
-detokenize lm (RectangleP id w h lw st)  = RectangleP id w h lw st
-detokenize lm (EllipseP id w h lw st)    = EllipseP id w h lw st
-detokenize lm (RowP id rf press)         = RowP id rf $ map (detokenize lm) press
-detokenize lm (ColP id rf f press)       = ColP id rf f $ map (detokenize lm) press
-detokenize lm (OverlayP id (pres:press)) = OverlayP id (detokenize lm pres : (map castPresToLay press)) -- cast is safe, no tokens in press
-detokenize lm (WithP ar pres)            = WithP ar $ detokenize lm pres
-detokenize lm (StructuralP id pres)      = StructuralP id $ detokenize lm pres
-detokenize lm (LocatorP l pres)          = LocatorP l $ detokenize lm pres
-detokenize lm (GraphP id d w h es press) = GraphP id d w h es $ map (detokenize lm) press
-detokenize lm (VertexP id v x y o pres)  = VertexP id v x y o $ detokenize lm pres
-detokenize lm (FormatterP id press)      = FormatterP id $ map (detokenize lm) press
-detokenize lm pr                         = debug Err ("Layout.detokenize: can't handle "++ show pr) $ castPresToLay pr
+detokenize wm (EmptyP id)                = EmptyP id
+detokenize wm (StringP id str)           = StringP id str
+detokenize wm (ImageP id str st)         = ImageP id str st
+detokenize wm (PolyP id pts w st)        = PolyP id pts w st
+detokenize wm (RectangleP id w h lw st)  = RectangleP id w h lw st
+detokenize wm (EllipseP id w h lw st)    = EllipseP id w h lw st
+detokenize wm (RowP id rf press)         = RowP id rf $ map (detokenize wm) press
+detokenize wm (ColP id rf f press)       = ColP id rf f $ map (detokenize wm) press
+detokenize wm (OverlayP id (pres:press)) = OverlayP id (detokenize wm pres : (map castPresToLay press)) -- cast is safe, no tokens in press
+detokenize wm (WithP ar pres)            = WithP ar $ detokenize wm pres
+detokenize wm (StructuralP id pres)      = StructuralP id $ detokenize wm pres
+detokenize wm (LocatorP l pres)          = LocatorP l $ detokenize wm pres
+detokenize wm (GraphP id d w h es press) = GraphP id d w h es $ map (detokenize wm) press
+detokenize wm (VertexP id v x y o pres)  = VertexP id v x y o $ detokenize wm pres
+detokenize wm (FormatterP id press)      = FormatterP id $ map (detokenize wm) press
+detokenize wm pr                         = debug Err ("Layout.detokenize: can't handle "++ show pr) $ castPresToLay pr
 
 
 -- find out semantics of this one        What about Refs?
 
 -- incomplete, only for strings
 detokenize' :: Show token => WhitespaceMap -> Presentation doc node clip token -> [Layout doc node clip]
-detokenize' lm (StructuralP id pres)      = [StructuralP id $ detokenize lm pres]
-detokenize' lm (EmptyP id)                = [EmptyP id]
-detokenize' lm (StringP id str)           = [StringP id str] -- addWhitespace lm id str
-detokenize' lm (TokenP id token)          = addWhitespaceToken lm id token
-detokenize' lm (ImageP id str st)         = [ImageP id str st]
-detokenize' lm (PolyP id pts w st)        = [PolyP id pts w st]
-detokenize' lm (RectangleP id w h lw st)  = [RectangleP id w h lw st]
-detokenize' lm (EllipseP id w h lw st)    = [EllipseP id w h lw st]
-detokenize' lm (RowP id rf press)         = detokenizeRow' lm press -- ref gets lost
-detokenize' lm (ColP id rf f press)       = [ColP id rf f $ concat (map (detokenize' lm) press)]
-detokenize' lm (OverlayP id (pres:press)) = let press' = detokenize' lm pres -- cast is safe, no tokens in press
+detokenize' wm (StructuralP id pres)      = [StructuralP id $ detokenize wm pres]
+detokenize' wm (EmptyP id)                = [EmptyP id]
+detokenize' wm (StringP id str)           = [StringP id str] -- addWhitespace wm id str
+detokenize' wm (TokenP id token)          = addWhitespaceToken wm id token
+detokenize' wm (ImageP id str st)         = [ImageP id str st]
+detokenize' wm (PolyP id pts w st)        = [PolyP id pts w st]
+detokenize' wm (RectangleP id w h lw st)  = [RectangleP id w h lw st]
+detokenize' wm (EllipseP id w h lw st)    = [EllipseP id w h lw st]
+detokenize' wm (RowP id rf press)         = detokenizeRow' wm press -- ref gets lost
+detokenize' wm (ColP id rf f press)       = [ColP id rf f $ concat (map (detokenize' wm) press)]
+detokenize' wm (OverlayP id (pres:press)) = let press' = detokenize' wm pres -- cast is safe, no tokens in press
                                             in  [ OverlayP id (pres' : map castPresToLay press) | pres' <- press' ]
-detokenize' lm (WithP ar pres)            = let press = detokenize' lm pres 
+detokenize' wm (WithP ar pres)            = let press = detokenize' wm pres 
                                             in  map (WithP ar) press
-detokenize' lm (ParsingP id l pres)       = let press = detokenize' lm pres 
+detokenize' wm (ParsingP id l pres)       = let press = detokenize' wm pres 
                                             in  map (ParsingP id l) press
-detokenize' lm (LocatorP l pres)          = let press = detokenize' lm pres 
+detokenize' wm (LocatorP l pres)          = let press = detokenize' wm pres 
                                             in  map (LocatorP l) press
-detokenize' lm (GraphP id d w h es press) = let press' = map (singleton . detokenize' lm) press
+detokenize' wm (GraphP id d w h es press) = let press' = map (singleton . detokenize' wm) press
                                             in  [GraphP id d w h es press']
-detokenize' lm (VertexP id v x y ol pres) = [VertexP id v x y ol (singleton $ detokenize' lm pres)]
-detokenize' lm (FormatterP id press)      = [FormatterP id $ concat (map (detokenize' lm) press) ]
-detokenize' lm pr                         = debug Err ("Layout.detokenize': can't handle "++ show pr) [castPresToLay pr]
+detokenize' wm (VertexP id v x y ol pres) = [VertexP id v x y ol (singleton $ detokenize' wm pres)]
+detokenize' wm (FormatterP id press)      = [FormatterP id $ concat (map (detokenize' wm) press) ]
+detokenize' wm pr                         = debug Err ("Layout.detokenize': can't handle "++ show pr) [castPresToLay pr]
 
 singleton []       = debug Err ("TreeEditPres.detokenize': graph child without singleton token (add row to presentation)") $ EmptyP NoIDP
 singleton [pres]   = pres
 singleton (pres:_) = debug Err ("TreeEditPres.detokenize': graph child without singleton token (add row to presentation)") $ pres
 
 detokenizeRow' :: Show token => WhitespaceMap -> [Presentation doc node clip token] -> [Layout doc node clip]
-detokenizeRow' lm [] = []
-detokenizeRow' lm (pres:press) =
-  let press' = detokenize' lm pres
-      press'' = detokenizeRow' lm press                         
+detokenizeRow' wm [] = []
+detokenizeRow' wm (pres:press) =
+  let press' = detokenize' wm pres
+      press'' = detokenizeRow' wm press                         
   in combine press' press''
 
 combine :: [Presentation doc node clip token] -> [Presentation doc node clip token] -> [Presentation doc node clip token]
@@ -88,17 +86,17 @@ combine l1 [] = l1
 combine l1 l2 = init l1 ++ [RowP NoIDP 0 $ [last l1,head l2] ] ++ tail l2
 
 addWhitespace :: WhitespaceMap -> IDP -> String -> [Layout doc node clip]
-addWhitespace lm NoIDP str = [StringP NoIDP str]
-addWhitespace lm id str = 
-  case Map.lookup id lm of
+addWhitespace wm NoIDP str = [StringP NoIDP str]
+addWhitespace wm id str = 
+  case Map.lookup id wm of
     Nothing -> [StringP id str]
     Just (breaks, spaces) ->    replicate breaks (StringP NoIDP "") 
                              ++ [StringP id (replicate spaces ' ' ++ str)]
 
 addWhitespaceStruct :: WhitespaceMap -> IDP -> Layout doc node clip -> [Layout doc node clip]
-addWhitespaceStruct lm NoIDP struct = [struct]
-addWhitespaceStruct lm id struct = 
-  case Map.lookup id lm of
+addWhitespaceStruct wm NoIDP struct = [struct]
+addWhitespaceStruct wm id struct = 
+  case Map.lookup id wm of
     Nothing -> [struct]
     Just (breaks, spaces) ->    replicate breaks (StringP NoIDP "") 
                              ++ [RowP NoIDP 0 [ StringP NoIDP (replicate spaces ' ')
@@ -106,17 +104,17 @@ addWhitespaceStruct lm id struct =
                                              ]]
                                 
 addWhitespaceToken :: Show token => WhitespaceMap -> IDP -> Token doc node clip token -> [Layout doc node clip]
-addWhitespaceToken lm id (UserTk _ str _ _) = addWhitespace lm id str
-addWhitespaceToken lm id (StructuralTk _ pres _ _) = addWhitespaceStruct lm id (detokenize lm pres)
+addWhitespaceToken wm id (UserTk _ str _ _) = addWhitespace wm id str
+addWhitespaceToken wm id (StructuralTk _ pres _ _) = addWhitespaceStruct wm id (detokenize wm pres)
 
 
 
 
 {-
 onlyWhitespace :: WhitespaceMap -> IDP -> [ Presentation ]
-onlyWhitespace lm NoIDP = []
-onlyWhitespace lm id = 
-  case Map.lookup lm id of
+onlyWhitespace wm NoIDP = []
+onlyWhitespace wm id = 
+  case Map.lookup wm id of
     Nothing -> []
     Just (breaks, spaces) ->    replicate breaks (StringP NoIDP "") 
                              ++ [StringP NoIDP (replicate spaces ' ')]
