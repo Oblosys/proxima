fb1d1679c8e4225e8d9f85120a6f5a9bcab3ed40
Oblosys <martijn@oblomov.com>
2008-03-08 16:14:04 +0000

    AG data type is generated correctly
    git-svn-id: svn://svn.oblomov.com/Proxima@1261 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

---
+++ generator/src/Gen_PresentationAG.hs
@@ -0,0 +1,42 @@
+-----------------------------------------------------------------------------------------
+{-| Module      : Gen_PresentationAG
+    Copyright   : 
+    License     : All Rights Reserved
+
+    Maintainer  : 
+    Stability   : 
+    Portability : 
+-}
+-----------------------------------------------------------------------------------------
+
+module Gen_PresentationAG where
+
+{-
+RUI's ipd computation was wrong:
+node without children
+should have
+ lhs.pIdC = @lhs.pIdC + n
+ 
+ Anyway, this is too buggy, so we should use FreshID and fill it in after presenting. (maybe during Layout)
+-}
+
+import TypesUtils
+
+import List
+
+generate :: DocumentType -> [String]
+generate docType = genDataType (addHolesParseErrs docTypeWithLists ++ genConsListDecls docType)
+  where docTypeWithLists = docType ++ genListDecls docType
+
+genDataType decls = addBanner "AG data type" $
+  concatMap genDataDecl decls
+ where genDataDecl (Decl _ typeName prods) = 
+         "DATA %1" <~ [typeName] :
+         [ "  | " ++ genProd prod | prod <- prods] ++
+         [ "", "" ]
+        where genProd (Prod cnstrName idpFields fields) =
+                cnstrName ++ (prefixBy " " $ map genIDPField idpFields ++
+                                             map genField fields)
+                      
+       genIDPField (Field fieldName fieldType) = fieldName ++ ":" ++ genIDPTypeAG  fieldType
+       genField    (Field fieldName fieldType) = fieldName ++ ":" ++ genTypeAG fieldType
