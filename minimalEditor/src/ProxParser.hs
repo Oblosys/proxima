86b976ba3cfd7e616b3ea370204d6b4b55ab8c8e
Oblosys <martijn@oblomov.com>
2008-03-17 17:22:36 +0000

    automatic structure recognizer: works for presentations that are entirely structural
    git-svn-id: svn://svn.oblomov.com/Proxima@1420 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- minimalEditor/src/ProxParser.hs
+++ minimalEditor/src/ProxParser.hs
@@ -63,34 +63,88 @@ parseTree =
   <|>     (\str -> reuseLeaf [str])
       <$> pToken LeafToken
 
-recognize :: Token Document Node ClipDoc UserToken -> ClipDoc
-recognize (StructuralTk _ (Just node) _ childTokens _) = 
-  let thisPath = pathNode node
+{-
+TODO
+handle wrong order
+handle missing children (reuse)
+handle parsing add not to parsingTk and add parser too (parser to clip?)
+-}
+
+{- recognize groups the children according to their paths. 
+-}
+recognize :: (Clip clip, Construct doc node clip token, DocNode node, Show token) =>
+             Token doc node clip token -> clip
+recognize strTk@(StructuralTk _ (Just node) _ childTokens _) = 
+  let thisPath = case pathNode node of
+                   PathD path -> path
+                   NoPathD    -> error $ "recognize: Encountered StructuralTk that has node without path:" ++ show strTk
+      numberedChildTokens = map (tokenPath thisPath) childTokens
+      constructorArity = arityClip result -- no problem, since the constructor can be evaluated lazily
+      initChildTokenGroups = replicate constructorArity []
+      childTokenGroups = addChildTokens initChildTokenGroups numberedChildTokens 
+      clipGroups       = map (map recognize) childTokenGroups
+      reuseArgs = [ case group of
+                      (clip:_) -> Just clip -- cannot handle multiple occurrences yet. Now we just take
+                                            -- the first, in the future, use dirty bits to take the updated one
+                      []       -> Nothing
+                  | group <- clipGroups
+                  ]
       parsedChildren = map recognize childTokens
-      result = construct node parsedChildren
-  in  debug Prs ("\nThis path"++ show thisPath ++"\nChildren: " ++ show (map tokenPath childTokens)) $
+      result = construct node strTk reuseArgs
+  in  debug Prs ("\nThis path"++ show thisPath ++"\nChildren (max "++show constructorArity++"):\n"++show numberedChildTokens++"\n" ++ show childTokenGroups) $
       result
+recognize strTk@(StructuralTk _ Nothing _ childTokens _) =
+  error $ "recognize: Encountered StructuralTk without node: " ++ show strTk
 
-
-tokenPath (StructuralTk _ (Just node) _ _ _) = pathNode node
 -- ParsingTk does not have a node! We have to add it.
+tokenPath :: (Construct doc node clip token, DocNode node, Show token) => Path -> Token doc node clip token -> (Int, Token doc node clip token)
+tokenPath parentPath strTk@(StructuralTk _ (Just node) _ _ _) = 
+  case pathNode node of
+    PathD path -> if parentPath `isPrefixOf` path 
+                  then case drop (length parentPath) path of
+                         [childNr] -> (childNr, strTk)
+                         _ -> error $ "encountered token that is not a child: tokenPath=" ++show path ++ 
+                                      " parentPath=" ++ show parentPath ++ " token=" ++ show strTk
+                  else error $ "encountered token that is not a child: tokenPath=" ++show path ++ 
+                                      " parentPath=" ++ show parentPath ++ " token=" ++ show strTk
+    NoPathD    -> error $ "tokenPath: childToken has node without path: " ++ show strTk
+tokenPath parentPath strTk@(StructuralTk _ Nothing _ _ _) = 
+  error $ "tokenPath: childToken without node" ++ show strTk
 
-construct (Node_Bin _ _) = construct_Tree_Bin 
-construct (Node_Leaf _ _) = construct_Tree_Leaf
-construct (Node_RootEnr _ _) = construct_EnrichedDoc_RootEnr
-construct x = error $ show x
+addChildTokens childTokenGroups []  = childTokenGroups
+addChildTokens childTokenGroups (childToken: childTokens) =
+  addChildTokens (addChildToken childTokenGroups childToken) childTokens
 
-construct_EnrichedDoc_RootEnr :: [ ClipDoc ] -> ClipDoc
-construct_EnrichedDoc_RootEnr [clip1] = let Just c1 = fromClip clip1
-                                        in  Clip_EnrichedDoc $ RootEnr c1
+addChildToken childTokenGroups (nr, childToken) =
+  case splitAt nr childTokenGroups of
+    (left, group:right) -> left ++ (group ++ [childToken]) : right
+    _                   -> error $ "addChildToken: encountered child with number larger than parents arity: nr="++show nr ++ " token="++show childToken 
 
-construct_Tree_Bin :: [ ClipDoc ] -> ClipDoc
-construct_Tree_Bin (clip1: clip2:_) = let Just c1 = fromClip clip1
-                                          Just c2 = fromClip clip2
-                                    in  Clip_Tree $ Bin c1 c2
-construct_Tree_Leaf [] = Clip_Tree Leaf
+      
+-- if the argument is nothing, return nothing (reuse), otherwise apply fromClip to the clip
+retrieveArg (Just clip) = case fromClip clip of
+                  Just x  -> Just x
+                  Nothing -> error "retrieveArg: Type error"
+retrieveArg Nothing     = Nothing
+
+construct_EnrichedDoc_RootEnr :: Token Document Node ClipDoc UserToken -> [ Maybe ClipDoc ] -> ClipDoc
+construct_EnrichedDoc_RootEnr tk ~[mclip1] = Clip_EnrichedDoc $ reuseRootEnr [tk] (retrieveArg mclip1)
+
+construct_Tree_Bin :: Token Document Node ClipDoc UserToken -> [Maybe ClipDoc ] -> ClipDoc
+construct_Tree_Bin  tk ~(clip1: clip2:_) = Clip_Tree $ reuseBin [tk] (retrieveArg clip1) (retrieveArg clip2)
+construct_Tree_Leaf tk ~[] = Clip_Tree $ reuseLeaf [tk] 
   
 
+
+
+class Construct doc node clip token where
+  construct :: node -> (Token doc node clip token) -> [Maybe clip] -> clip
+
+instance Construct Document Node ClipDoc UserToken where
+  construct (Node_Bin _ _) = construct_Tree_Bin 
+  construct (Node_Leaf _ _) = construct_Tree_Leaf
+  construct (Node_RootEnr _ _) = construct_EnrichedDoc_RootEnr
+  construct x = error $ show x
 
 class Clipable a where 
   toClip :: a -> ClipDoc
