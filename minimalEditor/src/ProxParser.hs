3992d996254db285d430ee0c2321fdb1bd8c03f6
Oblosys <martijn@oblomov.com>
2008-03-17 11:50:41 +0000

    playing with automatic structure recognizers
    git-svn-id: svn://svn.oblomov.com/Proxima@1408 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- minimalEditor/src/ProxParser.hs
+++ minimalEditor/src/ProxParser.hs
@@ -1,4 +1,4 @@
-module ProxParser (recognizeRootEnr) where
+module ProxParser (recognizeEnrichedDoc) where
 
 import Common.CommonTypes hiding (Dirty (..))
 import qualified Common.CommonTypes
@@ -24,12 +24,21 @@ import DocUtils_Generated
               
 -------------------- Proxima Parser/Structure Recognizer -------------------- 
 
+
 recognizeRootEnr :: ListParser Document Node ClipDoc UserToken EnrichedDoc
 recognizeRootEnr = pStr $ 
           (\str root-> reuseRootEnr [str] (Just root))
       <$> pStructural Node_RootEnr
---      <*> recognizeTree
-      <*> pPrs parseTree
+      <*> recognizeTree
+--      <*> pPrs parseTree
+
+recognizeEnrichedDoc :: ListParser Document Node ClipDoc UserToken EnrichedDoc
+recognizeEnrichedDoc =  
+          (\str -> let clip = recognize str
+                       in  case fromClip clip of 
+                              Just enr -> enr
+                              Nothing   -> error $ "Error"++show clip )
+      <$> pStructural Node_RootEnr
       
 recognizeTree :: ListParser Document Node ClipDoc UserToken Tree
 recognizeTree = pStr $
@@ -37,6 +46,7 @@ recognizeTree = pStr $
       <$> pStructural Node_Bin
       <*> recognizeTree
       <*> recognizeTree
+      <*  recognizeTree
   <|>     (\str -> reuseLeaf [str])
       <$> pStructural Node_Leaf
 
@@ -53,3 +63,44 @@ parseTree =
   <|>     (\str -> reuseLeaf [str])
       <$> pToken LeafToken
 
+recognize :: Token Document Node ClipDoc UserToken -> ClipDoc
+recognize (StructuralTk (Just node) _ childTokens _) = 
+  let thisPath = pathNode node
+      parsedChildren = map recognize childTokens
+      result = construct node parsedChildren
+  in  debug Prs ("\nThis path"++ show thisPath ++"\nChildren: " ++ show (map tokenPath childTokens)) $
+      result
+
+
+tokenPath (StructuralTk (Just node) _ _ _) = pathNode node
+-- ParsingTk does not have a node! We have to add it.
+
+construct (Node_Bin _ _) = construct_Tree_Bin 
+construct (Node_Leaf _ _) = construct_Tree_Leaf
+construct (Node_RootEnr _ _) = construct_EnrichedDoc_RootEnr
+construct x = error $ show x
+
+construct_EnrichedDoc_RootEnr :: [ ClipDoc ] -> ClipDoc
+construct_EnrichedDoc_RootEnr [clip1] = let Just c1 = fromClip clip1
+                                        in  Clip_EnrichedDoc $ RootEnr c1
+
+construct_Tree_Bin :: [ ClipDoc ] -> ClipDoc
+construct_Tree_Bin (clip1: clip2:_) = let Just c1 = fromClip clip1
+                                          Just c2 = fromClip clip2
+                                    in  Clip_Tree $ Bin c1 c2
+construct_Tree_Leaf [] = Clip_Tree Leaf
+  
+
+
+class Clipable a where 
+  toClip :: a -> ClipDoc
+  fromClip :: ClipDoc -> Maybe a
+  
+instance Clipable Tree where
+  toClip t = Clip_Tree t
+  fromClip (Clip_Tree t) = Just t
+  fromClip _             = Nothing
+instance Clipable EnrichedDoc where
+  toClip t = Clip_EnrichedDoc t
+  fromClip (Clip_EnrichedDoc t) = Just t
+  fromClip _             = Nothing
