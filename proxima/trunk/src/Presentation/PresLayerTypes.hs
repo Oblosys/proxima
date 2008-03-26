371044fdd2090ec44ec01a3fb643e28c63b3bb05
Oblosys <martijn@oblomov.com>
2008-03-26 10:11:47 +0000

    removed old UU_Parsing import
    git-svn-id: svn://svn.oblomov.com/Proxima@1515 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- proxima/trunk/src/Presentation/PresLayerTypes.hs
+++ proxima/trunk/src/Presentation/PresLayerTypes.hs
@@ -7,8 +7,6 @@ import Common.CommonTypes
 import Evaluation.EnrTypes hiding (DocumentLevel)
 import Presentation.PresTypes
 
-import Common.UU_Parsing
-
 type LayerStatePres = ()
 
 type PresentationSheet doc enr node clip token = enr -> FocusDoc -> WhitespaceMap -> IDPCounter -> 
