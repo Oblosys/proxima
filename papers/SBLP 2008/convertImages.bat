41bd7e95288cc75e5c8cf474d92876a57c91353e
Oblosys <martijn@oblomov.com>
2008-03-30 17:11:02 +0000

    fixed convertImages
    git-svn-id: svn://svn.oblomov.com/Proxima@1555 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- papers/SBLP 2008/convertImages.bat
+++ papers/SBLP 2008/convertImages.bat
@@ -1,7 +1,2 @@
-cd TexSources\Pics
-for  %%f  in  (*.png)   do d:\bin\imagemagick\convert.exe   %%f eps\%%f.eps 
-cd ..\..
-
-cd TexSources\Pics\Screenshots
-for  %%f  in  (*.png)   do d:\bin\imagemagick\convert.exe %%f %%f.eps 
-cd ..\..\..
+bash convertImages
+pause
