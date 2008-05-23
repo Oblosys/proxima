bf87e88b2d390ee25766ff37e451a946338855a7
Oblosys <martijn@oblomov.com>
2008-05-23 13:09:05 +0000

    Using generics to setProbtables
    git-svn-id: svn://svn.oblomov.com/Proxima@1693 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- dazzleEditor/src/Evaluator.hs
+++ dazzleEditor/src/Evaluator.hs
@@ -7,6 +7,8 @@ import Evaluation.EvalLayerUtils
 import DocTypes_Generated
 import DocUtils_Generated
 
+import Data.Generics
+
 instance EvaluationSheet Document EnrichedDoc ClipDoc where
   evaluationSheetSimplest (RootDoc root)        = RootEnr $ evaluateRoot root
   evaluationSheetSimplest HoleDocument          = HoleEnrichedDoc
@@ -14,31 +16,18 @@ instance EvaluationSheet Document EnrichedDoc ClipDoc where
 
 -- The extra doc parameter is necessary for popups in the current version of Proxima
 
+
 evaluateRoot (Root graph probtables title sections) =
-  Root graph probtables title (setProbtablesSectionList probtableMap sections)
+  Root graph probtables title (everywhere (mkT (setProbtablePara probtableMap)) sections)
  where probtableMap = map probtableMapEntry (fromList_Probtable probtables)
        probtableMapEntry probtable@(Probtable id _ _) = (id,probtable)
 
 type ProbtableMap = [(Int, Probtable)]
 
-setProbtablesSectionList :: ProbtableMap -> List_Section -> List_Section
-setProbtablesSectionList probtableMap sections = 
-  toList_Section $ setProbtablesSections probtableMap $ fromList_Section sections
-
-setProbtablesSections probtableMap []                                           = []
-setProbtablesSections probtableMap (Section title paras subsections : sections) = 
-  Section title 
-          (toList_Paragraph $ setProbtablesParas probtableMap (fromList_Paragraph paras))
-          subsections :
-  setProbtablesSections probtableMap sections     
-
-
-setProbtablesParas probtableMap [] = []
-setProbtablesParas probtableMap (ProbtablePara (Probtable id _ _):paras) = 
+setProbtablePara probtableMap (ProbtablePara (Probtable id _ _)) = 
   let probtable' = case lookup id probtableMap of
                      Just probtable -> probtable
                      Nothing        -> Probtable id (toList_Value []) $
                                          Table [] (toList_Axis []) (toList_Probability [])
-  in  ProbtablePara probtable' : setProbtablesParas probtableMap paras
-setProbtablesParas probtableMap (para:paras)                    = 
-  para : setProbtablesParas probtableMap paras
+  in  ProbtablePara probtable'
+setProbtablePara probtableMap para = para
