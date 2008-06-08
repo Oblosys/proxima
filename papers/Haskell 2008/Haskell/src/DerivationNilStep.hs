1c9000029ce5928308eab16d3de4f53bd600056d
Oblosys <martijn@oblomov.com>
2008-06-08 19:04:49 +0000

    git-svn-id: svn://svn.oblomov.com/Proxima@1740 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- papers/Haskell 2008/Haskell/src/DerivationNilStep.hs
+++ papers/Haskell 2008/Haskell/src/DerivationNilStep.hs
@@ -32,11 +32,11 @@ newtype Step a b ns = Step (a -> (b, ns))
 
 newtype NilStep t = NilStep t
 
+type Layer doc pres gest upd = Fix (Step doc pres  :.: Step gest upd :.: NilStep)
 --------------------------------------------------------
 lift0, lift1, lift2, lift4, lift5, lift, lift'  
       :: Simple state map doc pres gest upd -> state ->
-         Fix (Step doc pres  :.: Step gest upd :.: NilStep)
-
+         Layer doc pres gest upd
 
 lift0 simple state = step1 state 
  where step1 hArg = Fix . Comp . Step $
@@ -72,7 +72,7 @@ lift2 simple state =
  where step1 next hArg = Fix $  
          liftStep (present simple) next hArg
        step2 next hArg =
-         liftStep `(interpret simple) next hArg 
+         liftStep (interpret simple) next hArg 
       
 lift4 simple = fix (step1 . step2 . lNilStep)
  where step1 next hArg = Fix $
@@ -80,16 +80,18 @@ lift4 simple = fix (step1 . step2 . lNilStep)
        step2 next hArg =
          liftStep (interpret simple) next hArg
 
-lfix f = fix f'  where f' n =  (Fix . (f . lNilStep) n) 
+lfix f = fix f' where f' n = Fix . (f . lNilStep) n
 
-lift5 simple =  lfix (step1 . step2)
- where step1 next hArg = liftStep (present simple)  next hArg
-       step2 next hArg = liftStep (interpret simple) next hArg
+lift5 simple = lfix (step1 . step2)
+ where step1 next hArg = 
+         liftStep (present simple)  next hArg
+       step2 next hArg = 
+         liftStep (interpret simple) next hArg
        
-lift simple =  lfix (liftStep (present simple) . liftStep (interpret simple))
 
   
-lift simple =  lfix $ liftStep (present simple) . liftStep (interpret simple)
+lift simple = lfix $ liftStep (present simple) 
+                   . liftStep (interpret simple)
 
 
 -- or with a composition that removes left step. No need for liftStep's, but does need the lNilStep
@@ -102,31 +104,32 @@ lfix' f = fix f'  where f' n =  (Fix . f n)
 lift' simple =  lfix' $ present simple .: interpret simple .: lNilStep
 
 combine0, combine1, combine2, combine3, combine5, combine::
-  Layer2 high med emed ehigh ->
-  Layer2 med low elow emed -> 
-  Layer2 high low elow ehigh 
+        Layer high med emed ehigh ->
+        Layer med low elow emed -> 
+        Layer high low elow ehigh 
 
 combine0 upr lwr = step1 upr lwr
  where step1 (Fix (Comp (Step upr))) 
-                (Fix (Comp (Step lwr))) = 
+             (Fix (Comp (Step lwr))) = 
          Fix . Comp . Step $ 
          \high -> let (med, uprIntr) = upr high
                       (low, lwrIntr) = lwr med
                   in  (low, step2 uprIntr lwrIntr)
-       step2 (Comp (Step upr)) (Comp (Step lwr)) = Comp . Step $
+       step2 (Comp (Step upr)) (Comp (Step lwr)) = 
+         Comp . Step $
          \low -> let (med, lwrPres) = lwr low
                      (high, uprPres) = upr med
                  in  (high, cNilStep uprPres lwrPres)
-       cNilStep (NilStep u) (NilStep l) = NilStep $ step1 u l 
+       cNilStep (NilStep u) (NilStep l) = 
+         NilStep $ step1 u l 
 
 
 
 
-cNilStep next (NilStep u) (NilStep l) = NilStep $ next u l 
  
 combine1 upr lwr = (step1 (step2 (cNilStep combine1))) upr lwr
  where step1 next (Fix (Comp (Step upr))) 
-                (Fix (Comp (Step lwr))) = 
+                  (Fix (Comp (Step lwr))) = 
          Fix . Comp . Step $ 
          \high -> let (med, uprIntr) = upr high
                       (low, lwrIntr) = lwr med
@@ -137,45 +140,57 @@ combine1 upr lwr = (step1 (step2 (cNilStep combine1))) upr lwr
                  in  (high, next uprPres lwrPres)
  
 
-combine2 upr lwr = fix (step1 . step2  . cNilStep) upr lwr
+combine2 upr lwr = fix (step1 . step2 . cNilStep) upr lwr
  where step1 next (Fix (Comp (Step upr))) 
-                (Fix (Comp (Step lwr))) = 
+                  (Fix (Comp (Step lwr))) = 
          Fix . Comp . Step $ 
          \high -> let (med, uprIntr) = upr high
                       (low, lwrIntr) = lwr med
                   in  (low, next uprIntr lwrIntr)
-       step2 next (Comp (Step upr)) (Comp ( Step lwr)) = Comp . Step $
+       step2 next (Comp (Step upr)) (Comp ( Step lwr)) = 
+         Comp . Step $
          \low -> let (med, lwrPres) = lwr low
                      (high, uprPres) = upr med
                  in  (high, next uprPres lwrPres)
 
-combineStepDown :: (f x -> g y -> h ns) ->
-               (Step a b :.: f) x -> (Step b c :.: g) y -> (Step a c :.: h) ns
+cNilStep next (NilStep u) (NilStep l) = 
+  NilStep $ next u l
+
+combineStepDown :: (f x -> g y -> h ns) -> (Step a b :.: f) x -> 
+                   (Step b c :.: g) y -> (Step a c :.: h) ns
 combineStepDown next (Comp (Step upper)) (Comp (Step lower)) = Comp . Step $
   \h -> let (m ,upperf) = upper h
             (l, lowerf) = lower m
         in  (l, next upperf lowerf)   
 
-combineStepUp :: (f x -> g y -> h ns) ->
-               (Step b c :.: f) x -> (Step a b :.: g) y -> (Step a c :.: h) ns
+combineStepUp :: (f x -> g y -> h ns) -> (Step b c :.: f) x ->
+                 (Step a b :.: g) y -> (Step a c :.: h) ns
 combineStepUp next (Comp (Step upper)) (Comp (Step lower)) = Comp . Step $ 
   \l -> let (m, lowerf) = lower l
             (h, upperf) = upper m
         in  (h, next upperf lowerf)   
 
-combine3 upr lwr = fix (step1 . step2  . cNilStep) upr lwr
- where step1 next (Fix upr) 
-                (Fix lwr) = 
+combine3 upr lwr = fix (step1 . step2 . cNilStep) upr lwr
+ where step1 next (Fix upr) (Fix lwr) = 
          Fix $ combineStepDown next upr lwr
        step2 = combineStepUp
 
+
+combine4 = fix (\n (Fix u) (Fix l) -> 
+  Fix $ (step1 . combineStepUp . cNilStep) n u l) 
+ where step1 next upr lwr = 
+         combineStepDown next upr lwr
+
+
 cfix f = fix f' 
-  where f' n (Fix u) (Fix l) = Fix $ (f . cNilStep) n u l
+ where f' n (Fix u) (Fix l) = Fix $ (f . cNilStep) n u l
 
 combine5 upr lwr = cfix (step1 . step2) upr lwr
  where step1 next upr lwr = 
          combineStepDown next upr lwr
        step2 = combineStepUp
+
+
 
 combine = cfix (combineStepDown . combineStepUp)
 
@@ -185,9 +200,24 @@ combine = cfix (combineStepDown . combineStepUp)
 
 
 
-
-
 -- testing
+unStep (Comp (Step step)) = step
+unNil (NilStep step) = step
+
+editLoop (Fix presentStep) doc = 
+ do { let (pres , interpretStep) = 
+            unStep presentStep $ doc
+    
+    ; showRendering pres
+    ; gesture <- getGesture
+    
+    ; let (update, presentStep') =
+            unStep interpretStep $ gesture
+    
+    ; let doc' = updateDocument update doc
+    ; 
+    ; editLoop (unNil presentStep') doc'
+    }
 
                 
 type Layer2 a b a2 b2 = Fix (Step a b :.: Step a2 b2 :.: NilStep)
