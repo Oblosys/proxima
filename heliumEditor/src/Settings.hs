ddf0d5e025f36fb6c1895a87e1f828ec048bbfc9
Oblosys <martijn@oblomov.com>
2008-07-09 17:57:58 +0000

    added Settings module
    git-svn-id: svn://svn.oblomov.com/Proxima@1812 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

---
+++ heliumEditor/src/Settings.hs
@@ -0,0 +1,27 @@
+-----------------------------------------------------------------------------------------
+{-| Module      : Settings
+    Copyright   : (c) 2007 Martijn Schrage
+    License     : All Rights Reserved
+
+    Maintainer  : martijn@cs.uu.nl
+    Stability   : experimental
+    Portability : 
+-}
+-----------------------------------------------------------------------------------------
+
+module Settings (settings) where
+
+import Common.CommonTypes
+
+-- Constants for switching on incrementality, background coloring, etc.
+                
+settings =
+  Settings { rendererIncrementality = False
+           , arrangerIncrementality = False
+
+             -- use a smaller rectangle as viewed area to see what happens outside it
+           , reducedViewedArea = False            
+
+             -- updated parts of the rendering are surrounded by red rectangles
+           , markUpdatedRenderingArea = False
+           }
