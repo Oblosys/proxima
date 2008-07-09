f66289eabcae4103bce6ff5268658bb4f7af0910
Oblosys <martijn@oblomov.com>
2008-07-09 18:04:56 +0000

    added Settings module
    git-svn-id: svn://svn.oblomov.com/Proxima@1815 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

---
+++ minimalEditor/src/Settings.hs
@@ -0,0 +1,33 @@
+-----------------------------------------------------------------------------------------
+{-| Module      : Settings
+    Copyright   : (c) 2007 Martijn Schrage
+    License     : All Rights Reserved
+
+    Maintainer  : martijn@cs.uu.nl
+    Stability   : experimental
+    Portability : 
+-}
+-----------------------------------------------------------------------------------------
+
+module Settings (settings) where
+
+import Common.CommonTypes
+
+-- Constants for switching on incrementality, special markings in the rendering, etc.
+
+-- Settings for the ArrangerAG are still defined in CommonUtils, since it would add to the
+-- run-time cost of the arranger to pass the record around as an inherited attribute.
+
+                
+settings =
+  Settings { applicationName = "Dazzle documentation editor"
+
+           , rendererIncrementality = False
+           , arrangerIncrementality = False
+
+             -- use a smaller rectangle as viewed area to see what happens outside it
+           , reducedViewedArea = False            
+
+             -- updated parts of the rendering are surrounded by red rectangles
+           , markUpdatedRenderingArea = True
+           }
