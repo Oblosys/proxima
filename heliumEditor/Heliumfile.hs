30fd5e62e3023870728aea6f97b34c93cc4613d5
Oblosys <martijn@oblomov.com>
2008-11-03 13:50:07 +0000

    git-svn-id: svn://svn.oblomov.com/Proxima@1955 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- heliumEditor/Heliumfile.hs
+++ heliumEditor/Heliumfile.hs
@@ -1,1 +1,14 @@
-x = 1%2;
+h = [(1,False),(2,True),(3,False)];
+
+increase = \x -> x + 1;
+
+s = \f -> \g -> \x -> f x (g x);
+
+test = f True;
+
+c = 1;
+
+x = let local = 1+2+3+4; h = 1; in if True then local + h else 0;
+
+f = \x -> x^2+2*x+(3+x)*(2+x)*1%(x+1)^2;
+
