dbad94103e0dbb8f7597d2b6e0f4f77716c4a501
Oblosys <martijn@oblomov.com>
2009-01-19 14:53:16 +0000

    removed old server
    git-svn-id: svn://svn.oblomov.com/Proxima@1991 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- proxima/trunk/src/Proxima/Server.hs
+++ proxima/trunk/src/Proxima/Server.hs
@@ -1,8 +1,16 @@
-{-# OPTIONS -fglasgow-exts #-}
+module Proxima.Server where
 
-{-# LANGUAGE TemplateHaskell #-}
-module Server where
+import Common.CommonTypes ( DebugLevel (..), debug, showDebug, showDebug', debugIO, debugLnIO
+                          , Settings (..) )
+import qualified Common.CommonTypes as CommonTypes
+import Common.CommonUtils
+import Rendering.RenTypes
+import System.IO
+import Data.IORef
+import Proxima.Wrap
 
+import Data.Time.Clock
+import Control.Exception
 
 import HAppS.Server
 import HAppS.Server.SimpleHTTP
@@ -10,13 +18,28 @@ import HAppS.State
 import System.Environment
 import Control.Concurrent
 import System.Time
-import Data.Typeable
 import Control.Monad.Trans
 import Control.Monad
 import Data.List
-import HAppS.Data.Xml.HaXml
+import Data.Char
 
-import Text.XHtml.Strict hiding (method)
+import Data.List
+import Evaluation.DocTypes (DocumentLevel, EditDocument'_ (..))
+import Arrangement.ArrTypes
+import Presentation.PresTypes (UpdateDoc)
+import HAppS.Server
+import HAppS.State
+import System.Environment
+import Control.Concurrent
+import System.Time
+import Data.Typeable hiding (cast)
+import Control.Monad.Trans
+import Control.Monad hiding (when)
+import Data.List
+
+import Text.XHtml.Strict hiding (method, header)
+
+
 
 
 
@@ -27,8 +50,7 @@ GET /handle?commands=Key(116,(False,False,False));
 -}
 
 
-
-serverHAppS params = withProgName "proxima" $
+server params = withProgName "proxima" $
  do { initR <- newIORef (True)
     ; menuR <- newIORef []
 
@@ -93,59 +115,6 @@ data Commands = Commands String deriving Show
 
 instance FromData Commands where
   fromData = liftM Commands (look "commands")
-
-serverLoop params initR menuR serverSocket = loop $
- do { connection <- accept serverSocket
-    ; putStrLn $ "\nNew connection" ++ show connection
-    ; serve params initR menuR connection 
-    } `Control.Exception.catch` \err -> 
- do { putStrLn $ "Socket handler terminated:\n"++ show err
-    }
-           
-serve params initR menuR (handle, remoteHostName, portNumber) =
- do {
-    ; hSetBuffering handle LineBuffering
-    ; putStrLn $ "Connected to "++show remoteHostName ++ " on port " ++ show portNumber
-    
-    ; loop (handleRequest params initR menuR handle)
-    }
-
-handleRequest (settings,handler,renderingLvlVar,viewedAreaRef) initR menuR handle =
- do { commandLines <- hGetLinez handle
-    ; fh <- openFile "rendering.html" WriteMode
-    ; hClose fh
-    
-    ; putStrLn $ "Handling on socket: " ++ show handle
---    ; mapM putStrLn commandLines
-    
-    ; let arg = (takeWhile (/=' ') (drop 5 (head commandLines)))
-    ; putStrLn $ "arg (len="++show (length arg) ++"):\n" ++ take 70 arg
-    ; if arg == ""  
-      then
-       do { writeIORef viewedAreaRef ((0,0),(1000,800)) -- todo: take this from an init event
-          ; page <- readFile "../proxima/scripts/Editor.html" -- in Proxima tree, changes location when proxima is not in subdir
-          ; seq (length page) $ return ()
-          -- ; print page
-          ; hPutStr handle $ toHTTP page
-          ; hFlush handle
-          }
-      else if arg == "favicon.ico" then
-       do { hPutStr handle $ httpNotFound
-          ; hFlush handle
-          }
-      else if "img/" `isPrefixOf` arg then handleImage handle arg
-      else if "handle?commands="  `isPrefixOf` arg
-           then do { result <- handleCommands (settings,handler,renderingLvlVar,viewedAreaRef) initR menuR
-                                              (Commands $ drop 16 arg) -- drop the "handle?command="
-                   ; hPutStr handle $ toHTTP $ result
-                   ; hFlush handle
-                   ; putStrLn $ "\n\n\n\ncmds = "++show (drop 16 arg)
-                   ; putStrLn $ "\n\n\nresponse = \n" ++ show result
-                             
-                   }
-      else  error "Unhandled request"
-    }
-
 
 splitCommands commandStr =
   case break (==';') commandStr of
@@ -233,21 +202,14 @@ handleCommand (settings,handler,renderingLvlVar,viewedAreaRef) initR menuR handl
               }
     }
 
-
 handleMetrics ('M':'e':'t':'r':'i':'c':'s':event) =
- do { let receivedMetrics :: ((String,Int),(Int,Int,[Int])) = read $ fromHTML event
+ do { let receivedMetrics :: ((String,Int),(Int,Int,[Int])) = read $ event
 --    ; putStrLn $ "Received metrics:"++show receivedMetrics
     ; fh' <- openFile "queriedMetrics.txt" AppendMode
     ; hPutStrLn fh' $ show receivedMetrics
     ; hClose fh'
     }
     
-fromHTML [] = []
-fromHTML ('%':'2':'0':cs) = ' ': fromHTML cs
-fromHTML ('%':'2':'2':cs) = '"': fromHTML cs -- "
-fromHTML ('%':'5':'B':cs) = '[': fromHTML cs
-fromHTML ('%':'5':'D':cs) = ']': fromHTML cs
-fromHTML (c:cs) = c:fromHTML cs
 -- Current structure of handlers causes focus to be repainted after context request
 -- this is not really a problem though
 handleContextMenuRequest renderingLvlVar menuR ('C':'o':'n':'t':'e':'x':'t':'R':'e':'q':'u':'e':'s':'t':event) =
@@ -269,10 +231,6 @@ handleContextMenuRequest renderingLvlVar menuR ('C':'o':'n':'t':'e':'x':'t':'R':
     ; hClose fh
     }
  
-{-
- 
-                           
--}
 handleContextMenuSelect :: forall doc enr clip node token .
                            IORef [UpdateDoc doc clip] -> String -> IO (EditRendering doc enr node clip token)
 handleContextMenuSelect menuR ('C':'o':'n':'t':'e':'x':'t':'S':'e':'l':'e':'c':'t':event) =
@@ -363,119 +321,6 @@ handleSpecial viewedAreaRef malEvent editStr focus =
     ; return $ SkipRen 0
     }
 
-handleImage handle ('i':'m':'g':'/':unsafeImageFilename) = 
- do { let imageFilename = [c | c <- unsafeImageFilename, c /= '/' && c /='\\' ]
-          extension = reverse . takeWhile (/='.') . reverse $ imageFilename
-    ; putStrLn $ "Image download: " ++ imageFilename
-    ; h <- openBinaryFile ("img/"++imageFilename) ReadMode 
-    ; bytes <- hGetContents h -- httpImage computes length, so whole file is read
-    ; hPutStr handle $ httpImage extension bytes
-    ; hFlush handle
-    ; hClose h
-    } `Control.Exception.catch`
-   \err -> 
-    do { putStrLn $ "Problem while uploading "++unsafeImageFilename++":\n"
-       ; hPutStr handle $ httpNotFound
-       ; hFlush handle
-       }         
-handleImage handle malEvent =
- do { putStrLn $ "Internal error: malformed image event: " ++ malEvent
-    }
-    
--- todo: figure out whether caching is sufficient
-httpImage extension bytes = httpImageHeader extension (length bytes) ++ bytes
-httpImageHeader extension len = 
-  unlines 
-    [ "HTTP/1.1 200 OK"
-    , "Date: Mon, 28 Jul 2008 11:24:47 GMT"
-    , "Server: Proxima"
-    , "Last-Modified: Tue, 23 Jan 2007 16:10:01 GMT"
-    --ETag: "179010-2d22-427b76900c840\""
-    , "Accept-Ranges: bytes"
-    , "Content-Length: "++show len
-    , "Keep-Alive: timeout=1000, max=100"
-    , "Connection: Keep-Alive"
-    , "Content-Type: image/"++extension
-    , ""
-    ]
--- todo: somehow this does not produce a 404 page on firefox.    
-httpNotFound = unlines $ header ++ page
- where header =
-         [ "HTTP/1.1 404 Not Found"
-         , "Date: Sat, 09 Aug 2008 15:41:47 GMT"
-         , "Server: Proxima"
-         , "Last-Modified: Fri, 11 Nov 2005 13:52:33 GMT"
-         , "ETag: \"288266-2d8-4054a6ee40a40\""
-         , "Accept-Ranges: bytes"
-         , "Content-Length: "++show (length page)
-         , "Keep-Alive: timeout=1000, max=100"
-         , "Connection: Keep-Alive"
-         , "Content-Type: text/html"
-         , ""
-         ]
-       page =
-         [ "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">"
-         , "<HTML>"
-         , "<HEAD>"
-         , "<TITLE>404 Not Found</TITLE>"
-         , "<META HTTP-EQUIV=\"content-type\" CONTENT=\"text/html; charset=ISO-8859-1\">"
-         , "</HEAD>"
-         , "<BODY>"
-         , "<H3>404 Not Found</H3>"
-         , "The document requested was not found on this server."
-         , "</BODY>"
-         , "</HTML>"
-         ]
-{-
-http request created by firefox for url: http://localhost/img/squiggly.png
-
---
-GET /img/squiggly.png HTTP/1.1
-Host: localhost
-User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.1) Gecko/2008070208 Firefox/3.0.1
-Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
-Accept-Language: en-us,en;q=0.5
-Accept-Encoding: gzip,deflate
-Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7
-Keep-Alive: 300
-Connection: keep-alive
-If-Modified-Since: Tue, 23 Jan 2007 16:10:01 GMT
-Cache-Control: max-age=0
-
---
-If-Modified-Since: is only added when image was requested before:) 
--}
-
-toHTTP string = unlines (header (length string)) ++ string
-header len =  
-  [ "HTTP/1.1 200 OK"
-  , "Date: Mon, 28 Jul 2008 11:24:47 GMT"
-  , "Server: Proxima"
-  , "Last-Modified: Wed, 16 Jul 2008 15:56:44 GMT"
-  , "Expires: Mon, 28 Jul 2000 11:24:47 GMT"
-  , "ETag: \"3a387-627-452120dff27aa\""
-  , "Accept-Ranges: bytes"
-  , "Content-Length: " ++ show len
-  , "Keep-Alive: timeout=1000, max=100"
-  , "Connection: Keep-Alive"
-  , "Content-Type: text/xml"
-  , ""
-  ]
-  
-
-html (focusX,focusY) status text =  
-  "<div id=\"info\" focusX=\""++show focusX ++"\" focusY=\""++show focusY ++
-                  "\" status=\""++status ++ "\"></div>" ++
-  concatMap htmlChar text ++
-  ""
- where htmlChar '\n' = "<br/>"
-       --htmlChar ' '  = "&#8194;"
-       htmlChar ' '  = "&nbsp;"
-       htmlChar '<'  = "&lt;"
-       htmlChar '>'  = "&gt;"
-       htmlChar c    = [c]
-
-
 genericHandlerServer :: Settings ->
                ((RenderingLevel doc enr node clip token, EditRendering doc enr node clip token) -> IO (RenderingLevel doc enr node clip token, [EditRendering' doc enr node clip token])) ->
                IORef (RenderingLevel doc enr node clip token) -> IORef CommonTypes.Rectangle -> 
@@ -515,35 +360,3 @@ drawFocusHTML settings renderingLvlVar viewedAreaRef =
     ; focusRenderingHTML viewedArea
     }
 
--- Utility functions
-
-    
-hGetLinez :: Handle -> IO [String]
-hGetLinez handle = 
- do { str <- hGetLine handle
-    -- ; putStrLn $ "Socket "++show handle ++": "++str
-    ; strs <- if str /= "\r"
-              then hGetLinez handle
-              else return []
-    ; return $ str:strs
-    }
-
-
-
-loop io = loop'
- where loop' = 
-        do { io
-           ; loop'
-           }
-           
-
-getHostIP :: IO String
-getHostIP = 
- do { hostName <- getHostName
-    ; hostEntry <- getHostByName hostName
-    ; let addr = hostAddress hostEntry
-          (b1,b2,b3,b4) = ( addr `div` 16777216 `mod` 256, addr `div` 65536 `mod` 256
-                          , addr `div` 256 `mod` 256, addr `mod` 256)
-    ; return $ show b4 ++ "." ++ show b3 ++ "." ++ show b2 ++ "." ++ show b1
-    }
-           
