75b6ef8feb5a7d28d6046d679357c45a8cd418cf
Oblosys <martijn@oblomov.com>
2009-02-18 14:35:51 +0000

    git-svn-id: svn://svn.oblomov.com/Proxima@2058 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- proxima/trunk/scripts/updateRemoteDazzleEditor.bat
+++ proxima/trunk/scripts/updateRemoteDazzleEditor.bat
@@ -1,4 +1,6 @@
 ssh martijn@pooh.zoo.cs.uu.nl killall -v -g DazzleEditor
 ssh martijn@pooh.zoo.cs.uu.nl killall -v -g startDazzleEditor
+ssh martijn@pooh.zoo.cs.uu.nl proxima/proxima/scripts/updateProxima
 ssh martijn@pooh.zoo.cs.uu.nl proxima/proxima/scripts/updateDazzleEditor
+ssh martijn@pooh.zoo.cs.uu.nl proxima/proxima/scripts/buildDazzleEditor
 pause
---
+++ proxima/trunk/scripts/updateRemoteProxima.bat
@@ -0,0 +1,2 @@
+ssh martijn@pooh.zoo.cs.uu.nl proxima/proxima/scripts/updateProxima
+pause
