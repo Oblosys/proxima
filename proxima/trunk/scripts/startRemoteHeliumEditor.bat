ae505fea7d8b5e7b5ea4996c38ed24826688c1a6
Oblosys <martijn@oblomov.com>
2009-02-20 12:38:18 +0000

    added scripts for building/starting helium editor on pooh
    git-svn-id: svn://svn.oblomov.com/Proxima@2071 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

---
+++ proxima/trunk/scripts/startRemoteHeliumEditor.bat
@@ -0,0 +1,4 @@
+ssh martijn@pooh.zoo.cs.uu.nl killall -v -g HeliumEditor
+ssh martijn@pooh.zoo.cs.uu.nl killall -v -g startHeliumEditor
+ssh martijn@pooh.zoo.cs.uu.nl "nohup ./proxima/proxima/scripts/startHeliumEditor &"
+pause
---
+++ proxima/trunk/scripts/updateBuildRemoteHeliumEditor.bat
@@ -0,0 +1,6 @@
+ssh martijn@pooh.zoo.cs.uu.nl killall -v -g HeliumEditor
+ssh martijn@pooh.zoo.cs.uu.nl killall -v -g startHeliumEditor
+ssh martijn@pooh.zoo.cs.uu.nl proxima/proxima/scripts/updateProxima
+ssh martijn@pooh.zoo.cs.uu.nl proxima/proxima/scripts/updateHeliumEditor
+ssh martijn@pooh.zoo.cs.uu.nl proxima/proxima/scripts/buildHeliumEditor
+pause
