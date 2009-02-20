acc68355c03c2ecf98b610e570a1c25f456b6505
Oblosys <martijn@oblomov.com>
2009-02-20 14:16:05 +0000

    finally seem to have fixed nasty ssh startup problems
    git-svn-id: svn://svn.oblomov.com/Proxima@2076 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- proxima/trunk/scripts/startRemoteHeliumEditor.bat
+++ proxima/trunk/scripts/startRemoteHeliumEditor.bat
@@ -1,4 +1,4 @@
 ssh martijn@pooh.zoo.cs.uu.nl killall -v -g HeliumEditor
 ssh martijn@pooh.zoo.cs.uu.nl killall -v -g startHeliumEditor
-ssh martijn@pooh.zoo.cs.uu.nl "nohup ./proxima/proxima/scripts/startHeliumEditor &"
+ssh martijn@pooh.zoo.cs.uu.nl "nohup ./proxima/proxima/scripts/startHeliumEditor < /dev/null > /dev/null 2>&1 &"
 pause
--- proxima/trunk/scripts/updateBuildRemoteDazzleEditor.bat
+++ proxima/trunk/scripts/updateBuildRemoteDazzleEditor.bat
@@ -1,6 +1,7 @@
 ssh martijn@pooh.zoo.cs.uu.nl killall -v -g DazzleEditor
 ssh martijn@pooh.zoo.cs.uu.nl killall -v -g startDazzleEditor
 ssh martijn@pooh.zoo.cs.uu.nl proxima/proxima/scripts/updateProxima
+ssh martijn@pooh.zoo.cs.uu.nl chmod u+x proxima/proxima/scripts/*
 ssh martijn@pooh.zoo.cs.uu.nl proxima/proxima/scripts/updateDazzleEditor
 ssh martijn@pooh.zoo.cs.uu.nl proxima/proxima/scripts/buildDazzleEditor
 pause
