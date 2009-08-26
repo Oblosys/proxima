98e653d1e2b658b643bc1a69c51664dcd6e3a759
Oblosys <martijn@oblomov.com>
2009-08-26 14:45:36 +0000

    wrap instead of cast
    git-svn-id: svn://svn.oblomov.com/Proxima@2363 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- proxima/trunk/src/Presentation/XprezLib.hs
+++ proxima/trunk/src/Presentation/XprezLib.hs
@@ -188,14 +188,12 @@ strikeOut xp = withFont_ xp (\f -> f { fStrikeOut = True })
 
 
 withMouseDown :: forall doc enr node clip token . 
-                 (Wrapable (Wrapped doc enr node clip token) doc enr node clip token) => 
                  Xprez doc enr node clip token -> UpdateDoc doc clip -> Xprez doc enr node clip token
-withMouseDown xp upd = withInh xp (\i -> i { mouseDown = Just $ cast (UpdateDoc' upd :: EditDocument' doc enr node clip token) })
+withMouseDown xp upd = withInh xp (\i -> i { mouseDown = Just $ wrap (UpdateDoc' upd :: EditDocument' doc enr node clip token) })
 
 withMouseDownBold :: forall doc enr node clip token . 
-                 (Wrapable (Wrapped doc enr node clip token) doc enr node clip token) => 
                  Xprez doc enr node clip token -> Xprez doc enr node clip token
-withMouseDownBold xp = withInh xp (\i -> i { mouseDown = Just $ cast (SetStyleLay Bold :: EditLayout doc enr node clip token) })
+withMouseDownBold xp = withInh xp (\i -> i { mouseDown = Just $ wrap (InsertLay 'X' :: EditLayout doc enr node clip token) })
 
 -- this one deletes all inherited popup items
 withInheritablePopupMenuItems :: Xprez doc enr node clip token -> [PopupMenuItem doc clip] -> Xprez doc enr node clip token
