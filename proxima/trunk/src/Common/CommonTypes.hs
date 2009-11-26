1965e22b9f649800f7190b7ac2302ab59a5ae0d7
Oblosys <martijn@oblomov.com>
2009-11-26 15:31:59 +0000

    disabled strictness in DiffTree
    git-svn-id: svn://svn.oblomov.com/Proxima@2562 ca55bdf3-f6e6-0310-b62a-ae6ec707ba67

--- proxima/trunk/src/Common/CommonTypes.hs
+++ proxima/trunk/src/Common/CommonTypes.hs
@@ -170,8 +170,8 @@ isSelfCleanDT (DiffLeaf c) = c
 isSelfCleanDT (DiffNode c c' _) = c'
 
 
-data DiffTreeArr = DiffLeafArr !Bool (Maybe MoveRen) 
-                 | DiffNodeArr !Bool !Bool (Maybe MoveRen) (Maybe InsertDeleteRen) ![DiffTreeArr]
+data DiffTreeArr = DiffLeafArr Bool (Maybe MoveRen) 
+                 | DiffNodeArr Bool Bool (Maybe MoveRen) (Maybe InsertDeleteRen) [DiffTreeArr]
 --                selfAndDescendents self 
 -- DiffTree may contain infinite lists, so don't use it to recurse on (use a path arrangement instead)
 
@@ -180,6 +180,7 @@ data DiffTreeArr = DiffLeafArr !Bool (Maybe MoveRen)
 -- basically, self dirty means that the subtree will be entirely redrawn
 
 type MoveRen = ((XCoord, YCoord), (Width, Height))
+-- move is relative, so it's more a move/resize
 
 data InsertDeleteRen = InsertChildrenRen Int Int
                      | DeleteChildrenRen Int Int deriving Show
