2e95a876bdd8d402d540d145468c540d2aa0def1
Martijn Schrage <martijn@oblomov.com>
2015-08-06 17:35:23 +0200

    Add Settings.hs versions for all 4 sub-editors
    Not ideal, but Proxima does not support command-line options, or even running multiple instances in a single directory. Instead, we copy a src/Settings-x.hs to src/Settings.hs and Document-x.xml to Document.xml to build the desired sub-editor.

---
+++ declarationForm/src/Settings-sudoku.hs
@@ -0,0 +1,34 @@
+-----------------------------------------------------------------------------------------
+{-| Module      : Settings
+    Copyright   : (c) 2007 Martijn Schrage
+    License     : All Rights Reserved
+
+    Maintainer  : martijn@cs.uu.nl
+    Stability   : experimental
+    Portability : 
+-}
+-----------------------------------------------------------------------------------------
+
+module Settings (settings) where
+
+import Common.CommonTypes
+
+-- Constants for switching on incrementality, special markings in the rendering, etc.
+
+-- Settings for the ArrangerAG are still defined in CommonUtils, since it would add to the
+-- run-time cost of the arranger to pass the record around as an inherited attribute.
+
+                
+settings = defaultSettings { applicationName = "Sudoku"
+
+                           , rendererIncrementality = True
+                           , arrangerIncrementality = True
+
+                               -- use a smaller rectangle as viewed area to see what happens outside it
+                           , reducedViewedArea = False            
+
+                               -- updated parts of the rendering are surrounded by red rectangles
+                           , markUpdatedRenderingArea = False
+                           , serverPort = 8084
+                           }
+           
